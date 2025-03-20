c2740d4839ff2083575865ccd444a199bd8eebb2
mtlha03 <muhdtlha3@gmail.com>
2025-03-20 21:10:03 +0500

    config server

--- server/server.js
+++ server/server.js
@@ -35,5 +35,7 @@ connectDB();
 app.get('/', (req, res)=>{
   res.send('Server is running') 
 })
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
