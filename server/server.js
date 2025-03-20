55f2c459a2c8bcd8fc77377813b96bed8a9c6e9b
mtlha03 <muhdtlha3@gmail.com>
2025-03-20 23:38:38 +0500

    server ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,46 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import connectDB from "./db/config.js";
+// import authRoutes from "./routes/authRoutes.js";
+// import CauthRoutes from "./routes/CauthRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import cartRoutes from "./routes/cartRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
+
+// dotenv.config();
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// const allowedOrigins = ["http://localhost:5173", "https://cartify-server-c09ra1uil-muhammad-talhas-projects-a3a7b800.vercel.app"];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// app.use("/api/auth", authRoutes);
+// app.use("/auth", CauthRoutes);
+// app.use("/product", productRoutes);
+// app.use("/cart", cartRoutes);
+// app.use("/order", orderRoutes);
+
+// connectDB();
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -15,7 +58,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173", "https://caritfy-server.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173", "https://cartify-server-c09ra1uil-muhammad-talhas-projects-a3a7b800.vercel.app"];
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -38,5 +81,4 @@ app.use("/order", orderRoutes);
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
