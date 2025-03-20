7455ae55653af9aaa80afff5d31a692b9ed6f8fd
mtlha03 <muhdtlha3@gmail.com>
2025-03-20 21:45:35 +0500

    server config

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173", "https://caritfymarketplace.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://caritfy-server.vercel.app/"];
 app.use(
   cors({
     origin: function (origin, callback) {
